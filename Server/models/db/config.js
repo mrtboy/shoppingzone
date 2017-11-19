e8bde6074dbe12381d7c5d0b087899f5724b6d12
mrtboy <mohammadreza_t2001@yahoo.com>
2017-11-19 02:21:05 +0100

    Add CRUD for Product and categories on Server

--- Server/models/db/config.js
+++ Server/models/db/config.js
@@ -2,5 +2,5 @@ import mongoose from 'mongoose';
 
 
 module.exports = app => {
-    app.mongoConnection = mongoose.connect('mongodb://localhost:27017/goozman', { useMongoClient: true });
+    app.mongoConnection = mongoose.connect('mongodb://localhost:27017/zone', { useMongoClient: true });
 }
---
+++ Server/repositories/ProductRepository.js
@@ -0,0 +1,104 @@
+import mongoose from 'mongoose';
+
+module.exports = app => {
+    const Product = app.models.Product;
+    var ProductModel = mongoose.model("Product", Product);
+
+    const Category = app.models.Product;
+    var CategoryModel = mongoose.model("Category", Category);
+    var categoryRepository = app.repositories.CategoryRepository;
+    var helper = app.models.db.helper;
+
+    return {
+        //Show Product
+        findById: (model, result) => {
+            let query = ProductModel.find(helper.getId(model),
+                function (err, product) {
+                    query.select(Product);
+                    query.exec(function (err, product) {
+                        try {
+                            return result(helper.success(product));
+                        }
+                        catch (err) {
+                            return result(helper.failed(err.message));
+                        }
+                    })
+                });
+        },
+
+        //Delete Product
+        deleteById: (model, result) => {
+            ProductModel.findByIdAndRemove({ _id: model.id },
+                function (err, product) {
+                    try {
+                        if (!err && product !== null) {
+                            return result(helper.success(product));
+                        } else {
+                            return result(helper.failed(err.message));
+                        };
+                    }
+                    catch (err) {
+                        return result(helper.failed(err.message));
+                    }
+
+                }
+            )
+        },
+
+        //Edit Product
+        putById: (model, params, result) => {
+            ProductModel.findByIdAndUpdate(helper.getId(params), model,
+                function (model) {
+                    try {
+                        return result(helper.success());
+                    }
+                    catch (err) {
+                        return result(helper.failed(err.message));
+                    }
+                });
+        },
+
+        //Add Product        
+        add: (model, result) => {
+            let product = new ProductModel();
+            let categoryId = {
+                id: model.category
+            }
+            categoryRepository.findById(categoryId, (result) => {
+                try {
+                    categoryId = result.data[0]._id;
+                }
+                catch (err) {
+                    return result(err.message);
+                }
+            });
+            
+            product.name = model.name;
+            product.price = model.price;
+            product.location.lat = model.lat
+            product.location.long = model.long
+            product.location.street = model.street
+            product.location.streetNo = model.streetNo
+            product.location.houseNo = model.houseNo
+            product.location.Plz = model.Plz
+            product.location.city = model.city
+            product.location.country = model.country
+            product.summary = model.summary
+            product.description = model.description
+            product.condition = model.condition
+            product.category = categoryId.id
+            product.available = model.available
+            product.createdOn = Date.now();
+            product.save();
+            try {
+                return result(helper.success(product));
+            }
+            catch (err) {
+                return result(helper.failed(err.message));
+            }
+
+        }
+    };
+
+
+};
