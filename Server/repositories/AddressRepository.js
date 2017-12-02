d57310d23296555feea3c1ee3be2ac9e4fd0a443
mrtboy <mohammadreza_t2001@yahoo.com>
2017-12-02 11:26:12 +0100

    Added Address mongo model and addressRepository, change the models folder name to mongo, update product model, make relation one to many for category to product

---
+++ Server/repositories/AddressRepository.js
@@ -0,0 +1,93 @@
+import mongoose from 'mongoose';
+
+module.exports = app => {
+    const Address = app.models.Address;
+    var AddressModel = mongoose.model("Address");
+    var helper = app.models.db.helper;
+
+    return {
+        //Get one Address
+        findById: (model, result) => {
+            let query = AddressModel.find(helper.getId(model), 
+            function(err, address){
+                query.select(Address);
+                query.exec(function (err, address){
+                    try
+                    {
+                        return result(helper.success(address));
+                    }
+                    catch(err)
+                    {
+                        return result(helper.failed(err.message));
+                    }
+                })
+            });
+        },
+
+        //Find all Categories
+        findAll: (model, result) => {   
+            let query = AddressModel.find({}, 
+            function(err, address){
+                query.select(Address);
+                query.exec(function (err, address){
+                    try
+                    {
+                        return result(helper.success(address));
+                    }
+                    catch(err)
+                    {
+                        return result(helper.failed(err.message));
+                    }
+                })
+            });
+        },
+
+        //Delete Address
+        deleteById: (model, result) => {
+            AddressModel.findByIdAndRemove({ _id: model.id },
+                function (err, address) {
+                    try {
+                        if (!err && address !== null) {
+                            return result(helper.success(address));
+                        } else {
+                            return result(helper.failed(err.message));
+                        };
+                    }
+                    catch (err) {
+                        return result(helper.failed(err.message));
+                    }
+
+                }
+            )
+        },
+
+        //Edit Address
+        putById: (model, params, result) => {
+            AddressModel.findByIdAndUpdate(helper.getId(params), model,
+                function (model) {
+                    try {
+                        return result(helper.success());
+                    }
+                    catch (err) {
+                        return result(helper.failed(err.message));
+                    }
+                });
+        },
+        
+        //Add Address
+        add: (model, result) => {
+            let address = new AddressModel();
+            address.name = model.name;
+            address.description = model.description;
+            address.createdOn = Date.now();
+            try {
+                address.save();
+                return result(helper.success(address));
+            }
+            catch(err) {
+                return result(helper.failed(err.message));
+            }
+            
+        }
+    };
+};
