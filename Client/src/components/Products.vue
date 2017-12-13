eab5d32ac09904e620b46cf247f447436042b569
shamsuzzamansadi <abmsadi06@gmail.com>
2017-12-13 20:44:52 +0100

    products page updated

--- Client/src/components/Products.vue
+++ Client/src/components/Products.vue
@@ -1,51 +1,30 @@
 <template>
 
   <div class="container">
-    <div class="row">
-
-      <div class="col-md-6">
-
-  <form>
-    <div class="form-group row">
-      <label for="inputEmail3" class="col-sm-4 col-form-label">Fisrtname</label>
-      <div class="col-sm-8">
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+      <div class="row">
+      <div class="col-sm-3">
+        <div class="card">
+          <img class="card-img-top" src="http://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c05474585.png" alt="Card image cap">
+          <div class="card-body">
+            <p class="card-text">This is the best Laptop at 80 euros! Das ist Sonderangebot!</p>
+            <button class="btn btn-primary btn-sm active">Product Details</button>
+            <button class="btn btn-primary btn-sm active">Add to Cart!</button>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputEmail3" class="col-sm-4 col-form-label">Lastname</label>
-      <div class="col-sm-8">
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-      </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputEmail3" class="col-sm-4 col-form-label">Area Code</label>
-      <div class="col-sm-8">
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-      </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputEmail3" class="col-sm-4 col-form-label">Email</label>
-      <div class="col-sm-8">
-        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-      </div>
-    </div>
-    <div class="form-group row">
-      <label for="inputPassword3" class="col-sm-4 col-form-label">Password</label>
-      <div class="col-sm-8">
-        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+        <div class="col-sm-3">
+          <div class="card">
+            <img class="card-img-top" src="https://www.msi.com/asset/resize/picture/us/news-s/news_1501543761597fbd5188382-png/405.jpg" alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text">This is the best Laptop for Programmers! Das kostet 800 euro!</p>
+              <button class="btn btn-primary btn-sm active">Product Details</button>
+              <button class="btn btn-primary btn-sm active">Add to Cart!</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="form-group row">
-      <div class="col-sm-12 col-md-6">
-        <button type="submit" class="btn btn-primary" style="width: 10rem" >Sign up</button>
-      </div>
-    </div>
-  </form>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
